9aec9d95fd1f5f06e15b9c9cb87e5e554b7e4041
ziad-bakki <bakkizia@msu.edu>
2025-01-05 19:15:40 +0300

    completed array cardio day 2

---
+++ 07 - Array Cardio Day 2/index-START.js
@@ -0,0 +1,34 @@
+var people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+var comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+var isAdult = people.some(function (person) { return ((new Date()).getFullYear()) - person.year >= 19; });
+console.log({ isAdult: isAdult });
+// Array.prototype.every() // is everyone 19 or older?
+var allAdults = people.every(function (person) { return ((new Date()).getFullYear()) - person.year >= 19; });
+console.log({ allAdults: allAdults });
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+var comment = comments.find(function (comment) { return comment.id === 823423; });
+console.log(comment);
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+var index = comments.findIndex(function (comment) { return comment.id === 823423; });
+comments.splice(index, 1);
+console.table(comments);
+var newComments = comments;
+newComments.splice(index, 2);
+console.table(newComments);
